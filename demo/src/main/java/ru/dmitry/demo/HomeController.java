1400c8b2e4617add5187d149034d991430c13867
Dmitry1232321 <cuznetsov.dmitri2003@yandex.ru>
2025-01-10 02:22:51 +0300

    add controller and view

---
+++ demo/src/main/java/ru/dmitry/demo/HomeController.java
@@ -0,0 +1,15 @@
+package ru.dmitry.demo;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class HomeController {
+
+    @GetMapping("/")
+    public String home() {
+        return "index";
+    }
+
+
+}
